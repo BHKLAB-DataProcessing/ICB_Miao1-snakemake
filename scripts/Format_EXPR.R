9add387287424216722acffca3ff3433ac050d7b
Minoru Nakano <minoru.nakano@gmail.com>
2022-08-05 16:08:08 -0400

    modified log transformation

--- scripts/Format_EXPR.R
+++ scripts/Format_EXPR.R
@@ -74,6 +74,6 @@ colnames(expr) = sapply( colnames(expr) , function(x){ unlist( strsplit( x , "_T
 
 
 case = read.csv( file.path(output_dir, "cased_sequenced.csv"), stringsAsFactors=FALSE , sep=";" )
-expr = log2( expr[ , case[ case$expr %in% 1 , ]$patient ] + 1 )
+expr = log2( expr[ , case[ case$expr %in% 1 , ]$patient ] + 0.001 )
 
 write.table( expr , file=file.path(output_dir, "EXPR.csv") , quote=FALSE , sep=";" , col.names=TRUE , row.names=TRUE )
